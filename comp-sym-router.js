1b350e74a39f44da75a326c534fd5866b3485b94
Dylan D'Costa <ddcosta595@gmail.com>
2018-04-08 12:47:54 -0600

    routing test 6

--- comp-sym-router.js
+++ comp-sym-router.js
@@ -4,28 +4,25 @@ module.exports = {
     defineRouting: function(app, mongoose, uristring)
     {
         mongoose.connect(uristring, function (err, res) {
-      if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      } else {
-      console.log ('Succeeded connected to: ' + uristring);
-      }
-    });
+            if (err) { console.log ('ERROR connecting to: ' + uristring + '. ' + err); } 
+            else { console.log ('Succeeded connected to: ' + uristring); }
+        });
         
         var compSchema = new mongoose.Schema({
-    id: Number,
-    symbol: String,
-    name: String,
-    sector: String,
-    subind: String,
-    addr: String,
-    dteAdd: String,
-    frqncy: Number
-});
+            id: Number,
+            symbol: String,
+            name: String,
+            sector: String,
+            subind: String,
+            addr: String,
+            dteAdd: String,
+            frqncy: Number
+        });
         
         var Comp = mongoose.model('companies', compSchema);
         
         app.route('/api/companies/:sym')
-        .get(function(req, resp) {
+            .get(function(req, resp) {
             Comp.find({symbol : req.params.sym}, function(err, data) { 
                 if (err) { resp.json({ message : 'Unable to find Companies' }); } 
                 else { resp.json(data); }
--- server.js
+++ server.js
@@ -13,27 +13,6 @@ var uristring =
     'mongodb://localhost/HelloMongoose';
 
 var theport = process.env.PORT || 5000;
-
-//mongoose.connect(uristring, function (err, res) {
-      //if (err) {
-      //console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      //} else {
-      //console.log ('Succeeded connected to: ' + uristring);
-      //}
-    //});
-
-//var compSchema = new mongoose.Schema({
-    //id: Number,
-    //symbol: String,
-    //name: String,
-    //sector: String,
-    //subind: String,
-    //addr: String,
-    //dteAdd: String,
-    //frqncy: Number
-//});
-
-//var Comp = mongoose.model('companies', compSchema);
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(parser.json());
