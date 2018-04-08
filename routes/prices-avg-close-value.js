224b1849a4e44dea49a64111c8fa60601f8032bb
ksant972@mtroyal.ca <ksant972@mtroyal.ca>
2018-04-08 15:31:56 -0600

    prices-avg-close-value

---
+++ routes/prices-avg-close-value.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = { 
+    defineRouting: function(app, mongoose, uristring)
+        {
+            mongoose.connect(uristring, function (err, res) {
+                if (err) { console.log ('ERROR connecting to: ' + uristring + '. ' + err); } 
+                else { console.log ('Succeeded connected to: ' + uristring); }
+            });
+
+            var schema = new mongoose.Schema({symbol: String, name: String});
+            var Comp = mongoose.model('avgClosePrice', 
+                                      new mongoose.Schema({collection: 'prices'}));
+
+            app.route('/api/prices/:sym/close')
+                .get(function(req, resp) {
+                Comp.find({name: req.params.sym, avgClose: { $avg: "close" }}, function(err, data) { 
+                    if (err) { resp.json({ message : 'Unable to find Companies' }); } 
+                    else { resp.json(data); }
+                });
+            });
+        }
+}
