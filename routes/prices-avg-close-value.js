b7e5a4f6b1b8770ceef864c6862abd3bd6b40700
ksant972@mtroyal.ca <ksant972@mtroyal.ca>
2018-04-08 22:55:27 -0600

    avg-close

--- routes/prices-avg-close-value.js
+++ routes/prices-avg-close-value.js
@@ -17,9 +17,9 @@ module.exports = {
                 Price.find({ name: req.params.sym}, function(err, data) {
                 if (err) { resp.json({ message : 'Unable to find prices' }); } 
                 else { 
-                        var priceData = _.map(data);
+                        var priceData = resp.json(_.map(data));
                         for(var price in priceData){
-                            price.date = price.date.split(date, "-");                        }
+                            price.date = price.date.split("-");                        }
 
                     resp.json(priceData); 
                      
