99d011453836872f8f8b749ea7fd9b291ae7b48a
ksant972@mtroyal.ca <ksant972@mtroyal.ca>
2018-04-09 20:01:53 -0600

    e

--- routes/price-router.js
+++ routes/price-router.js
@@ -21,8 +21,8 @@ module.exports = {
         });
         
         var priceClose = new mongoose.Schema({
-            name: String,
-            close: Number
+            close: Number,
+            name: String
         })
         
         var Price = mongoose.model('prices', priceSchema);
@@ -47,7 +47,7 @@ module.exports = {
         
         app.route('/api/prices/avgclose/:sym')
             .get(function(req, resp){
-            Close.find({name: req.param.sym}, function(err, data) {
+            Price.find({name: req.params.sym}, function(err, data) {
                 if (err) { resp.json({ message : 'Unable to find prices' }); } 
                 else { resp.json(_.map(data));}
             });
