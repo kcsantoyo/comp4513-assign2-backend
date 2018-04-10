e3dc906b722f1eb455ef60bdeec07d4dd116ef40
ksant972@mtroyal.ca <ksant972@mtroyal.ca>
2018-04-09 22:02:32 -0600

    e

--- routes/price-router.js
+++ routes/price-router.js
@@ -32,9 +32,9 @@ module.exports = {
             });
         });
         
-        app.route('/api/prices/:sym/:date')
+        app.route('/api/prices/:sym/')
             .get(function(req, resp) {
-            Price.find({name: req.params.sym, date: req.params.date}, function(err, data){
+            Price.find({name: req.params.sym}, function(err, data){
                 if (err) { resp.json({ message : 'Unable to find prices' }); } 
                 else { resp.json(data); }
             })
